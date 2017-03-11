f8ca9dc0d8124b2398ec0d612637439e6fd8ded3
monkeywu <james20209@msn.com>
2017-03-11 15:28:24 +0800

    b

--- about/js/about.js
+++ about/js/about.js
@@ -7,10 +7,19 @@ $('document').ready(function(){
 			$('.about').css({'height':'60px','border-bottom':'1px solid #333'})
 		}
 		if($(this).scrollTop()<790){
-			$('.percent').css({'width':'0%',})
+			$('.p1,.p2,.p3,.p4').css({'width':'0%',})
 		}
 		if($(this).scrollTop()>790){
-			$('.percent').css({'width':'80%',})
+			$('.p1').css({'width':'80%',})
+		}
+		if($(this).scrollTop()>790){
+			$('.p2').css({'width':'90%',})
+		}
+		if($(this).scrollTop()>790){
+			$('.p3').css({'width':'75%',})
+		}
+		if($(this).scrollTop()>790){
+			$('.p4').css({'width':'82%',})
 		}
 	})
 })
